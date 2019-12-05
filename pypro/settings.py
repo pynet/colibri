506ddc14ad78084df64892639ed3b00a9b0dd2c6
joaby <joabyaugusto@hotmail.com>
2019-12-05 16:12:47 -0300

    commit

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h-q(--jba=d#h5y%3(-45m%8h5u+-+p42b86#56#@-#3w4c-e1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
