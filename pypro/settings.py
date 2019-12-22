661506c4798d39460d136c40799edab07165a514
joaby <joabyaugusto@hotmail.com>
2019-12-22 18:05:32 -0300

    commit

--- pypro/settings.py
+++ pypro/settings.py
@@ -139,7 +139,6 @@ if AWS_ACCESS_KEY_ID:
 
     AWS_DEFAULT_ACL = 'private'
 
-
     # Static Assets
     # ------------------------------------------------------------------------------
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
@@ -148,7 +147,7 @@ if AWS_ACCESS_KEY_ID:
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-# Upload Media Folder
+    # Upload Media Folder
     DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
     DEFAULT_S3_PATH = 'media'
     MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
